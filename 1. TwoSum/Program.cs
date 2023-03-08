6b2953a7119b094413a2e44dc82a67a588d2e9d9
Antonio <antonio.marinovvvv@gmail.com>
2023-03-08 18:44:05 +0200

    Add project files.

---
+++ 1. TwoSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = TwoSum(new int[] { 2, 5, 5, 10 }, 10);
+            Console.WriteLine(String.Join(" ", res));
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                var current = nums[i];
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    var next = nums[j];
+
+                    if (current + next == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
