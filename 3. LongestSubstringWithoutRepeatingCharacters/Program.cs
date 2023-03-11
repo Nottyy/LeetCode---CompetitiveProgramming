7936d7af727b235dd551d7766babba84dc7840c0
Antonio <antonio.marinovvvv@gmail.com>
2023-03-11 20:23:53 +0200

    LeetCode - 3. Length Of Longest Substring - Iterative - Hashset - Dictionary - Iterative

---
+++ 3. LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace _3.LongestSubstringWithoutRepeatingCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LengthOfLongestSubstring("aabaab!bb"));
+            // "abaa"
+            // dvgdabsr
+        }
+
+        public static int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 1)
+            {
+                return 1;
+            }
+            var hash = new HashSet<char>();
+            var indeces = new Dictionary<char, int>();
+            var max = 0;
+            var current = 0;
+            var startIndex = 0;
+
+            //   aabaab!bb
+            //   abcabcbb
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (hash.Contains(s[i]))
+                {
+                    if (current > max)
+                    {
+                        max = current;
+                    }
+                    current = i - 1 - indeces[s[i]];
+
+                    hash.Clear();
+
+                    for (int j = indeces[s[i]]; j < i; j++)
+                    {
+                        hash.Add(s[j]);
+                    }
+                }
+
+                hash.Add(s[i]);
+                indeces[s[i]] = i;
+                current++;
+            }
+
+            return current > max ? current : max;
+        }
+    }
+}
