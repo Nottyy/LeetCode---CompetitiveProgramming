0a380b29a7afe28cd1893f98e44d9761ec26fef8
Antonio <antonio.marinovvvv@gmail.com>
2023-03-28 22:24:12 +0300

    string indexOf

---
+++ 28. FindTheIndexOfTheFirstOccurrenceInAString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28.FindTheIndexOfTheFirstOccurrenceInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StrStr("popsod", "pss"));
+
+        }
+
+        public static int StrStr(string haystack, string needle)
+        {
+            var st = haystack.IndexOf(needle);
+            return st;
+        }
+    }
+}
