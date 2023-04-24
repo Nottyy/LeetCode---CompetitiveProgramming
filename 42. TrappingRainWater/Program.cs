0a84cee9fc732b1cfca03ab10116a72aacb03eb3
Antonio <antonio.marinovvvv@gmail.com>
2023-04-24 15:39:18 +0300

    two pointers, leetcode 42

--- 42. TrappingRainWater/Program.cs
+++ 42. TrappingRainWater/Program.cs
@@ -13,7 +13,7 @@ namespace _42.TrappingRainWater
         {
             // 4, 6, 5, 3, 2, 1, 2, 0, 0, 4, 6
             // 5, 3, 1, 2, 0, 0, 4, 0, 0, 6
-            var arr = new int[] { 4, 2, 0, 3, 2, 5 };
+            var arr = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
             Console.WriteLine(Trap(arr));
         }
 
