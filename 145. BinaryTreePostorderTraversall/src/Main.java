db23655fc629e4ba0cef0db1e80a1b4b16ca81d2
Antonio <antonio.marinovvvv@gmail.com>
2023-05-03 13:22:31 +0300

    fixes

--- 145. BinaryTreePostorderTraversall/src/Main.java
+++ 145. BinaryTreePostorderTraversall/src/Main.java
@@ -1,12 +1,29 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var node1 = new TreeNode(4);
+        var node2 = new TreeNode(5);
+        var node3 = new TreeNode(1);
+        var node4 = new TreeNode(10);
+        var node5 = new TreeNode(2);
+        var node6 = new TreeNode(3);
+
+        node1.right = node2;
+        node1.left = node3;
+        node2.right = node4;
+        node3.right = node5;
+        node5.right = node6;
+
+        var numbers = postorderTraversal(node1);
+        List<String> strs = numbers.stream().map(Object::toString).collect(Collectors.toList());
+
+        System.out.println(String.join(" ", strs));
     }
 
-    public List<Integer> postorderTraversal(TreeNode root) {
+    public static List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         DFS(root, res);
 
@@ -16,8 +33,8 @@ public class Main {
     private static void DFS(TreeNode root, List<Integer> res) {
         if (root != null){
             DFS(root.left, res);
-            DFS(root.right, res);
             res.add(root.val);
+            DFS(root.right, res);
         }
     }
 
