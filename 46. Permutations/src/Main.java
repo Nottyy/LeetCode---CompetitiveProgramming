931df2a3debe78539094815f0f53b804b7985b90
Antonio <antonio.marinovvvv@gmail.com>
2023-05-30 19:58:25 +0300

    backtracking - dfs - permutations

--- 46. Permutations/src/Main.java
+++ 46. Permutations/src/Main.java
@@ -1,43 +1,42 @@
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        var ans = permute(new int[]{1,2,3});
-        for (var s: ans){
-            var res = s.stream().map(Object::toString).collect(Collectors.toList());
-            System.out.println(res);
-        }
     }
 
-    public static List<List<Integer>> ans = new ArrayList<>();
+    public  List<List<Integer>> ans = new ArrayList<>();
 
-    public static List<List<Integer>> permute(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        List<Integer> result = new ArrayList<>();
+    public  List<List<Integer>> permute(int[] nums) {
+        HashSet<Character> set = new HashSet<>();
 
-        Backtracking(nums, set, result);
+        Backtracking(nums, set, "");
 
         return ans;
     }
 
-    private static void Backtracking(int[] nums, HashSet<Integer> set, List<Integer> result) {
-        if (result.size() == nums.length) {
-            ans.add(new ArrayList<>(result));
+    private  void Backtracking(int[] nums, HashSet<Character> set, String result) {
+        if (result.length() == nums.length) {
+            List<Integer> arr = new ArrayList<>();
+            for (int i = 0; i < result.length(); i++){
+                arr.add(Character.getNumericValue(result.charAt(i)));
+            }
+            ans.add(new ArrayList<>(arr));
             return;
         }
 
         for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i])) {
+            if (set.contains((char)nums[i])) {
                 continue;
             } else {
-                set.add(nums[i]);
-                result.add(nums[i]);
+                var ch = (char)nums[i];
+
+                set.add(ch);
+                result += nums[i];
                 Backtracking(nums, set, result);
-                result.remove(result.size() - 1);
-                set.remove(nums[i]);
+                result = result.substring(0, result.length() - 1);
+                set.remove(ch);
             }
         }
     }
